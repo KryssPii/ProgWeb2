b9d2f0031e44b59af8584958be0c485711476194
chris <christian.subasik@heig-vd.ch>
2026-09-22 11:53:58 +0200

    début exercice 4

--- test.js
+++ test.js
@@ -23,7 +23,7 @@ function comparA(x, y) {
     } else {
         return false;
     }
-    return (x, y)(x === y);
+    return (x, y), (x === y);
 }
 console.log(comparA(4.0, 4));
 
@@ -32,6 +32,20 @@ function comparB(a, b) {
         // x et y sont égaux (peut être -0 et 0) ou sont tous les deux NaN
         return a === b || (a !== a && b !== b);
     }
-    return (a, b)(a === b);
+    return (a, b), (a === b);
 }
 console.log(comparB(4, '4'));
+
+//Exercice 4 
+function numberOf(n) {
+    if (n % 2 == 0) {
+        for (let i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                return i;
+            }
+        }
+    } else {
+
+    }
+}
+console.log(numberOf(6));
